72ac494eed138dddeca98a1d8de0ec218235b8a2
p45 <p45@edu.itstep.lv>
2025-06-13 22:50:52 -1200

    Add a new function

--- ConsoleApplication16.3/ConsoleApplication16.3.cpp
+++ ConsoleApplication16.3/ConsoleApplication16.3.cpp
@@ -4,10 +4,14 @@ using namespace std;
 void NewFunc() {
 	cout << "Hello, it's my new func!";
 }
+void SecondFunc() {
+	cout << "It's already second func!" << endl;
+}
 void main()
 {
 	cout << "Hello World!\n";
 	cout << "We are connected!" << endl;
 
 	NewFunc();
+	SecondFunc();
 }
